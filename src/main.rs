50e6700846343c0dbbf464a2b1ff4b99f71451bd
Aleksandra Rubaj <ola@yway.pl>
2022-02-17 15:03:49 +0100

    accept command line argument path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
